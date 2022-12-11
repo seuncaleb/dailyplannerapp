b4170facc02aee512891db185115c0ab7d02e613
Caleb I <calebibejigba@gmail.com>
2022-12-11 12:38:58 +0000

    added inputed values to lacal storage and calls to screen on refresh

--- starter/script.js
+++ starter/script.js
@@ -1,121 +1,151 @@
-$(document).ready(function(){
 
     // variable that keeps track of time of the day 
-let timeOfDay = [
+    let timeOfDay = [
+        {time: "08",
+    meridian: "am",
+    reminder: "",
+    id: "0"},
+    
+    {time: "09",
+    meridian: "am",
+    reminder: "",
+    id: "1"},
+    
+    {time: "10",
+    meridian: "am",
+    reminder: "",
+    id: "2"},
+    
+    {time: "11",
+    meridian: "am",
+    reminder: "",
+    id: "3"},
+    
     {time: "12",
-meridian: "am",
-reminder: ""},
-
-{time: "13",
-meridian: "pm",
-reminder: ""},
-
-{time: "14",
-meridian: "pm",
-reminder: ""},
-
-{time: "15",
-meridian: "pm",
-reminder: ""},
-
-{time: "16",
-meridian: "pm",
-reminder: ""},
-
-{time: "17",
-meridian: "pm",
-reminder: ""},
-
-{time: "18",
-meridian: "pm",
-reminder: ""},
-
-{time: "19",
-meridian: "pm",
-reminder: ""},
-
-{time: "20",
-meridian: "pm",
-reminder: ""},
-]
-
-// get time of the current date fot the header 
-
-$('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
-
-
-// display time a save button and allow a text area to type in reminders 
-
-    timeOfDay.forEach(function(time){
-        
-        //timeblock row
-        var rowHour = $("<form>").attr("class", "row");
-        $(".container").append(rowHour);
-
-   //time fields
-
-var timeSpace = $("<div>");
-timeSpace.text(`${time.time}${time.meridian}`)
-timeSpace.attr( "class", "col-md-2 hour");
-
-
-//schedule data 
-
-var hourPlan = $("<div>");
-hourPlan.attr("class", "col-md-9 description p-0");
-
-var reminderData = $("<textarea>");
-hourPlan.append(reminderData);
-
-reminderData.attr("id", time.time);
-reminderData.css('width', '100%')
-
-if (time.time < moment().format("HH")) {
-reminderData.attr ("class", "past")
-
-} else if (time.time === moment().format("HH")) {
-reminderData.attr("class", "present")
-
-} else if (time.time > moment().format("HH")) {
-reminderData.attr("class", "future ")
-}
-
-
-// savebutton 
-
-var saveBt = $("<i class='far fa-save fa-lg'></i>")
-var saveReminder = $("<button>");
-   saveReminder.attr("class", "col-md-1 saveBtn");
-saveReminder.append(saveBt);
-
-rowHour.append(timeSpace, hourPlan, saveReminder);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+    meridian: "pm",
+    reminder: "",
+    id: "4"},
+    
+    {time: "13",
+    meridian: "pm",
+    reminder: "",
+    id: "5"},
+    
+    {time: "14",
+    meridian: "pm",
+    reminder: "",
+    id: "6"},
+    
+    {time: "15",
+    meridian: "pm",
+    reminder: "",
+    id: "7"},
+    
+    {time: "16",
+    meridian: "pm",
+    reminder: "",
+    id: "8"},
+    ]
+    
+    // get time of the current date fot the header 
+    
+    $('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
+    
+    // to save reminder in local storage
+    
+    function saveReminders() {
+        localStorage.setItem("timeOfDay", JSON.stringify(timeOfDay));
+    }
+    
+    
+    // to let user see data in local storage
+    
+    
+    function displayReminders() {
+       timeOfDay.forEach(function (forNow) {
+            $(`#${forNow.id}`).val(forNow.reminder);
+        })
+    }
+    
+    function view() {
+        var storedReminder = JSON.parse(localStorage.getItem("timeOfDay"));
+    
+        if (storedReminder) {
+            timeOfDay = storedReminder;
+        }
+    
+        saveReminders();
+        displayReminders();
+    }
+    
+    // display time a save button and allow a text area to type in reminders 
+    
+        timeOfDay.forEach(function(time){
+            
+            //timeblock row
+            var rowHour = $("<form>").attr("class", "row");
+            $(".container").append(rowHour);
+    
+       //time fields
+    
+    var timeSpace = $("<div>");
+    timeSpace.text(`${time.time}${time.meridian}`)
+    timeSpace.attr( "class", "col-md-2 hour");
+      
+    
+    //schedule data 
+    
+    var hourPlan = $("<div>");
+    hourPlan.attr("class", "col-md-9 description p-0");
+    
+    var reminderData = $("<textarea>");
+    hourPlan.append(reminderData);
+    
+    reminderData.attr("id", time.id);
+    reminderData.css('width', '100%')
+    
+    if (time.time < moment().format("HH")) {
+    reminderData.attr ("class", "past")
+    
+    } else if (time.time === moment().format("HH")) {
+    reminderData.attr("class", "present")
+    
+    } else if (time.time > moment().format("HH")) {
+    reminderData.attr("class", "future ")
+    }
+    
+    
+    // savebutton 
+    
+    var saveBt = $("<i class='far fa-save fa-lg'></i>")
+    var saveReminder = $("<button>");
+       saveReminder.attr("class", "col-md-1 saveBtn");
+    saveReminder.append(saveBt);
+    
+    rowHour.append(timeSpace, hourPlan, saveReminder);
+    
+    
+    });
+    
+    view();
+    
+    
+    
+    console.log(localStorage)
+    
+    $(".saveBtn").on("click", function(event) {
+        event.preventDefault();
+        var saveIndex = $(this).siblings(".description").children(".future").attr("id");
+        timeOfDay[saveIndex].reminder = $(this).siblings(".description").children(".future").val();
+        console.log(saveIndex);
+        saveReminders();
+        displayReminders();
+    })
+    
+    
+    
+    
+    
+    
+    
+    
